45f43ff160ab6be0ef0b599061723c3f2356056f
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-09-14 15:29:10 -0400

    Add Subclass Text
    Find out what sublcass this barb is

--- barbarian.py
+++ barbarian.py
@@ -3,7 +3,8 @@ class Barbarian:
 	def __init__(self, name):
 		self.name = name
 		self.level = 0
-		self. rage = 0
+		self.max_rage = 0
+		self.rage = 0
 		self.hit_dice = 0
 
 	def get_name(self):
@@ -13,7 +14,7 @@ class Barbarian:
 		return self.level
 
 	def get_rage(self):
-		return self.rage
+		return self.max_rage
 
 	def get_hit_dice(self):
 		return self.hit_dice
@@ -26,21 +27,22 @@ class Barbarian:
 
 	def set_rage(self, level):
 		if level < 3:
-			self.rage = 2
+			self.rage = self.max_rage = 2
 		elif 3 <= level < 6:
-			self.rage = 3
+			self.rage = self.max_rage = 3
 		elif 6 <= level < 12:
-			self.rage = 4
+			self.rage = self.max_rage = 4
 		elif 12 <= level < 20:
-			self.rage = 5
+			self.rage = self.max_rage = 5
 		elif level >= 20:
-			self.rage = 100
+			self.rage = self.max_rage = 100
 
 	def set_hit_dice(self, level):
 		self.hit_dice = level
 
 	def use_hit_dice(self, amount):
 		self.hit_dice -= amount
+
 
 	def create_barbarian(self):
 		name = input("What is the barbarian's name?\n")
@@ -51,6 +53,21 @@ class Barbarian:
 		player.set_rage(level)
 		player.set_hit_dice(level)
 		print(player.get_name(), player.get_level(), player.get_rage(), player.get_hit_dice())
+
+
+class Berserker(Barbarian):
+
+	def __init__(self, name):
+		self.frenzy = 0
+		Barbarian.__init__(self, name)
+
+	def set_frenzy(self):
+		self.frenzy += 1
+
+	def use_rage(self):
+		self.rage -= 1
+		self.frenzy += 1
+
 
 
 
