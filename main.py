312f5813b3ee65d84a9e348f508e1a8964578cea
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-09-14 17:50:45 -0400

    Add number of players. Add dict to hold players

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from barbarian import *
 
-###################################################Bool#################################################################
+# BOOL
 running = True
 
-##################################################VAR##################################################################
-player_class = input("What is this player's class?")
-player_class = player_class.capitalize()
-p1 = None
+# VAR
+player_dictionary = {}
+# ################################################Program###############################################################
+number_of_players = int(input("How many players are in your party?"))
+for x in range(number_of_players):
+	p1 = None
+	player_class = input("What is this player's class?")
+	player_class = player_class.capitalize()
+	if player_class == "Barbarian":
+		player_subclass = input("What is their subclass?")
+		player_subclass = player_subclass.capitalize()
+		if player_subclass == "Berserker":
+			p1 = Berserker.create_berserker_barbarian(p1)
+		elif player_subclass == "Ancestral":
+			p1 = AncestralGuardian.create_ancestral_barbarian(p1)
+		elif player_subclass == "Zealot":
+			p1 = Zealot.create_Zealot_barbarian(p1)
 
-#################################################Program###############################################################
-if player_class == "Barbarian":
-	player_subclass = input("What is their subclass?")
-	player_subclass = player_subclass.capitalize()
-	if player_subclass == "Berserker":
-		p1 = Berserker.create_berserker_barbarian(p1)
-	elif player_subclass == "Ancestral":
-		p1 = AncestralGuardian.create_ancestral_barbarian(p1)
-	elif player_subclass == "Zealot":
-		p1 = Zealot.create_Zealot_barbarian(p1)
+	player_dictionary['p' + str(x)] = p1
+
 
 while running:
 	task = input("What would you like to do?\n")
@@ -28,7 +33,3 @@ while running:
 
 	else:
 		running = False
-
-
-
-
