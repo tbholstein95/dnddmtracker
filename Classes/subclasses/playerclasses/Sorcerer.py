84ef537d11f22d779d847990da8c0e2d631e3e56
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-10-29 16:11:21 -0400

    Add dragonic sorcerer options

--- Classes/subclasses/playerclasses/Sorcerer.py
+++ Classes/subclasses/playerclasses/Sorcerer.py
@@ -99,7 +99,79 @@ class Sorcerer(FullCaster):
 	def use_sorcerous_restoration(self):
 		self.add_sorcery_points(4)
 
+class Draconic(Sorcerer):
+	def __init__(self):
+		Sorcerer.__init__(self)
 
+	def elemental_affinity_resistance(self):
+		if self.level >= 6:
+			self.use_sorcery_point(1)
+			print("Sorcerer has resistance to whatever spell damage type was for an hour")
+		else:
+			print("Sorcerer needs to be at least level 6")
 
+	def use_draconic_presence(self):
+		if self.level >= 18:
+			self.use_sorcery_point(5)
+			print("Aura of fear created in 60 feet for 1 minute")
+		else:
+			print("Sorcerer needs to be level 18 first")
 
+	def list_draconic_options(self):
+		selection = int(input("What action are you counting?\n" + "[1]: Create Spell Slot\n" + (
+
+			"[2]: Create Sorcery Point\n" + "[3]: Use Careful Spell\n" + "[4]: Use Distant Spell\n" +
+
+			"[5]: Use Empowered Spell \n" + "[6]: Use Extended Spell\n" + "[7]: Use Heightened Spell\n" +
+
+			"[8]: Use Quickened Spell\n" + "[9]Use Subtle Spell\n" + "[10]: Use Twinned Spell\n" +
+
+			"[11]: Use Sorcerous Restoration\n" + "[12]: Use Elemental Affinity" + "[13]: Use Dragonic Presence" +
+
+			"[14]: Use Hit Dice\n" + "[15]: Change Level\n" + (
+				"[16]: Exit\n"))))
+
+		if selection == 1:
+			self.create_spell_slot()
+		elif selection == 2:
+			self.create_sorcery_points()
+		elif selection == 3:
+			self.use_careful_spell()
+		elif selection == 4:
+			self.use_distant_spell()
+		elif selection == 5:
+			self.use_empowered_spell()
+		elif selection == 6:
+			self.use_extended_spell()
+		elif selection == 7:
+			self.use_heightened_spell()
+		elif selection == 8:
+			self.use_quickened_spell()
+		elif selection == 9:
+			self.use_subtle_spell()
+		elif selection == 10:
+			self.use_twinned_spell()
+		elif selection == 11:
+			self.use_sorcerous_restoration()
+		elif selection == 12:
+			self.elemental_affinity_resistance()
+		elif selection == 13:
+			self.use_draconic_presence()
+		elif selection == 14:
+			self.use_hit_dice()
+		elif selection == 15:
+			self.set_level()
+			self.set_hit_dice(self.get_level())
+		elif selection == 16:
+			print("Leaving")
+			return
+
+class Wild_Magic(Sorcerer):
+	def __init__(self):
+		tides_of_chaos = False
+		Sorcerer.__init__(self)
+
+	def use_bend_luck(self):
+		self.use_sorcery_point(2)
+		print("Used bend luck")
 
